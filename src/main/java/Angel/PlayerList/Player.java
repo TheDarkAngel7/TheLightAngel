7ff1c36027f3455a14ca301a45253eac32def860
TheDarkAngel <rburke5786@gmail.com>
2026-01-28 16:24:41 -0600

    Instead of Pulling the full list from Discord's gateway every time the Player object is constructed, we're querying from cache instead.

--- src/main/java/Angel/PlayerList/Player.java
+++ src/main/java/Angel/PlayerList/Player.java
@@ -6,59 +6,37 @@ import org.apache.commons.text.similarity.LevenshteinDistance;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class Player implements PlayerListLogic {
     private final Logger log = LogManager.getLogger(Player.class);
-    private final AtomicReference<Member> playerAccount = new AtomicReference<>(null);
-    private final AtomicBoolean hasDuplicates = new AtomicBoolean(false);
+    private final Member playerAccount;
 
     public Player(String searchName) {
-        AtomicReference<List<Member>> verifiedMembers = new AtomicReference<>();
         Guild guild = getGuild();
 
-        CountDownLatch finalCountDownLatch = new CountDownLatch(1);
-
-        guild.loadMembers().onSuccess(members -> {
-           List<Member> vM = new ArrayList<>();
-
-           members.forEach(m -> {
-               if (m.getRoles().contains(mainConfig.getMemberRole()) || m.getRoles().contains(mainConfig.getTeamRole())) {
-                   vM.add(m);
-               }
-           });
-           verifiedMembers.set(vM);
-           finalCountDownLatch.countDown();
-        });
-
-        try {
-            finalCountDownLatch.await();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        List<Member> verifiedMembers = guild.getMembers().stream().
+                filter(m -> m.getRoles().contains(mainConfig.getMemberRole()) ||
+                        m.getRoles().contains(mainConfig.getTeamRole())).toList();
 
         int index = 0;
         Member accountCandidate = null;
         int lowestScore = 50;
         LevenshteinDistance levenshteinDistance = new LevenshteinDistance();
         do {
-            int score = levenshteinDistance.apply(searchName.toLowerCase(), verifiedMembers.get().get(index).getEffectiveName().toLowerCase());
+            int score = levenshteinDistance.apply(searchName.toLowerCase(), verifiedMembers.get(index).getEffectiveName().toLowerCase());
 
             if (score < lowestScore) {
                 log.debug("Based on {} we're thinking {} is the best match with a score of {}",
-                        searchName, verifiedMembers.get().get(index).getEffectiveName(), score);
-                accountCandidate = verifiedMembers.get().get(index);
+                        searchName, verifiedMembers.get(index).getEffectiveName(), score);
+                accountCandidate = verifiedMembers.get(index);
                 lowestScore = score;
             }
             if (lowestScore == 0) break;
             index++;
 
         }
-        while (index < verifiedMembers.get().size());
+        while (index < verifiedMembers.size());
 
         log.info("Based on the Query of {}, Discord Account: {} (ID: {})", searchName, accountCandidate.getEffectiveName(), accountCandidate.getIdLong());
 
@@ -69,48 +47,44 @@ public class Player implements PlayerListLogic {
             do {
                 int score = levenshteinDistance.apply(searchName.replaceAll("_", "")
                                 .replaceAll("-", "").toLowerCase(),
-                        verifiedMembers.get().get(index).getEffectiveName().replaceAll("_", "")
+                        verifiedMembers.get(index).getEffectiveName().replaceAll("_", "")
                                 .replaceAll("-", "").toLowerCase());
 
                 if (score < lowestScore) {
                     log.debug("With underscores and dashes removed from search query {}, we're thinking {} is the best match with a score of {}",
-                            searchName, verifiedMembers.get().get(index).getEffectiveName(), score);
-                    accountCandidate = verifiedMembers.get().get(index);
+                            searchName, verifiedMembers.get(index).getEffectiveName(), score);
+                    accountCandidate = verifiedMembers.get(index);
                     lowestScore = score;
                 }
                 if (lowestScore == 0) break;
                 index++;
 
-            } while (index < verifiedMembers.get().size());
+            } while (index < verifiedMembers.size());
             log.info("Based on Query of {} with underscores and dashes removed, Discord Account: {} (ID: {})",
                     searchName, accountCandidate.getEffectiveName(), accountCandidate.getIdLong());
         }
 
-        playerAccount.set(accountCandidate);
+        this.playerAccount = accountCandidate;
 
     }
 
     public String getPlayerName() {
-        return playerAccount.get().getEffectiveName();
+        return playerAccount.getEffectiveName();
     }
 
     public Member getDiscordAccount() {
-        return playerAccount.get();
+        return playerAccount;
     }
 
     public boolean isCrewMember() {
-        return isCrewMember(playerAccount.get());
+        return isCrewMember(playerAccount);
     }
 
     public boolean isSupporter() {
-        return isSupporter(playerAccount.get());
+        return isSupporter(playerAccount);
     }
 
     public boolean isStaff() {
-        return isTeamMember(playerAccount.get());
-    }
-
-    public boolean hasDuplicates() {
-        return hasDuplicates.get();
+        return isTeamMember(playerAccount);
     }
 }
