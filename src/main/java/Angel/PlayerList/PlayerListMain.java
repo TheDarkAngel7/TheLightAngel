d5e212dc580ed12ec8af1fe1a7b9309fe9402651
TheDarkAngel <rburke5786@gmail.com>
2026-04-17 13:52:44 -0500

    Update to 1.6.26.1: Corrected some error messages in PlayerListMain but also reverted a line that involved looking up a player directly from a message's Member object, the issue with msg.getMember() is if it's from a DM that will return null.

--- src/main/java/Angel/PlayerList/PlayerListMain.java
+++ src/main/java/Angel/PlayerList/PlayerListMain.java
@@ -692,7 +692,7 @@ public class PlayerListMain extends ListenerAdapter implements BotAbuseLogic {
             Member m = getGuild().getMemberById(msg.getAuthor().getIdLong());
 
             if (m == null) {
-                msg.getChannel().sendMessageEmbeds(new MessageEntry("No Access", "**You Do Not have access to this command as you are not in the SAFE Crew Discord Server!",
+                msg.getChannel().sendMessageEmbeds(new MessageEntry("No Access", "**You Do Not have access to this command as you are not in the SAFE Crew Discord Server!**",
                         EmbedDesign.ERROR).getEmbed()).queue();
             }
             else {
@@ -794,7 +794,7 @@ public class PlayerListMain extends ListenerAdapter implements BotAbuseLogic {
                     }
                     catch (InvalidSessionException e) {
                         msg.getChannel().sendMessageEmbeds(
-                                new MessageEntry("Invalid Session", "**Unable to Find a Session from no search as there is either more than 1 session running, or no sessions at all.**",
+                                new MessageEntry("Invalid Session", "**Unable to Find a Session from that search, try again**",
                                         EmbedDesign.ERROR).getEmbed()).queue();
                     }
                     catch (IOException e) {
